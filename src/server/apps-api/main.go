9890959776da8f956ef19009803a69f8e82a7325
Azer Abdullaev (Like All) <like-all@yandex.com>
2015-04-08 15:58:29 +0300

    Handling duolingo endpoints

--- src/server/apps-api/main.go
+++ src/server/apps-api/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+        "fmt"
+        "github.com/go-martini/martini"
+)
 
 func main() {
+    m := martini.Classic()
     fmt.Println("bullshit")
+
+    m.Get("/duolingo/:name", func(params martini.Params) string {
+        return "alert(\"" + params["name"] + "\");"
+    })
+
+    m.RunOnAddr("localhost:8353")
 }
---
+++ src/server/apps-api/util.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+        "os/exec"
+        "encoding/json"
+)
+
+type LangStats struct {
+    Streak                  int
+    Language_string         string
+    Language                string
+    Level                   int
+    To_next_level           int
+    Points                  int
+    Learning                bool
+    Current_learning        bool
+    Sentences_translated    int
+}
+
+func getDuolingoStats(name string) []LangStats {
+    cmd := exec.Command("/usr/bin/duolingo-api.py", name)
+    stdout, err := cmd.Output()
+    if err != nil {
+        println(err.Error())
+    }
+    var languages []LangStats
+    err = json.Unmarshal([]byte(stdout), languages)
+
+    return languages
+}
