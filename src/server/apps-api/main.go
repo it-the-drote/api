0b71829627023d533f322960d87656379d84a6cf
Azer Abdullaev (Like All) <like-all@yandex.com>
2015-04-08 17:06:30 +0300

    Testing middleware execution

--- src/server/apps-api/main.go
+++ src/server/apps-api/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-        "fmt"
         "github.com/go-martini/martini"
 )
 
 func main() {
     m := martini.Classic()
-    fmt.Println("bullshit")
-
     m.Get("/duolingo/:name", func(params martini.Params) string {
-        return "alert(\"" + params["name"] + "\");"
+        duolingoLangs := getDuolingoStats(params["name"])
+        return "alert(\"" + duolingoLangs[0].Language + "\");"
     })
 
     m.RunOnAddr("localhost:8353")
