41c682934b745f8c7fa434175300530ebb61ab01
Like-all <like-all@yandex.com>
2015-12-27 01:23:32 +0200

    Apply css rule to the whole block

--- src/server/public/js-templates/instagram-api.js
+++ src/server/public/js-templates/instagram-api.js
@@ -1,7 +1,8 @@
 var style = document.createElement('style');
 document.head.appendChild(style);
 var sheet = document.styleSheets[0];
-sheet.insertRule(".instagram { text-align: center; margin: 0 auto; }", sheet.cssRules.length);
+sheet.insertRule("#instagramBlock { margin: 0 auto; }", sheet.cssRules.length);
+sheet.insertRule(".instagram { text-align: center; }", sheet.cssRules.length);
 sheet.insertRule(".instagram-pic { max-width: 100%; max-height: 100%; }", sheet.cssRules.length);
 var langContent = document.createElement('div');
 langContent.innerHTML = '{htmlcontent}';
