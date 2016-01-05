6094602761b70b1ee77e5f53755a10163366a135
like-all <like-all@yandex.com>
2016-01-05 16:37:49 +0300

    blah
    * blah
    * Introducing Last.fm API

---
+++ src/server/public/js-templates/lastfm-api.js
@@ -0,0 +1,10 @@
+var style = document.createElement('style');
+document.head.appendChild(style);
+var sheet = document.styleSheets[0];
+sheet.insertRule("#lastfmBlock { margin: 0 auto; }", sheet.cssRules.length);
+sheet.insertRule(".lastfm { text-align: center; }", sheet.cssRules.length);
+sheet.insertRule(".lastfm-pic { max-width: 100%; max-height: 100%; }", sheet.cssRules.length);
+var langContent = document.createElement('div');
+langContent.innerHTML = '{htmlcontent}';
+var langBlock = document.getElementById('lastfmBlock');
+langBlock.appendChild(langContent);
