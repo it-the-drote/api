5dec56b402ab9122a4e9f95b6799253e4e15342e
like-all <like-all@yandex.com>
2016-01-05 21:30:27 +0300

    badge number position fix

--- src/server/public/js-templates/duolingo-api.js
+++ src/server/public/js-templates/duolingo-api.js
@@ -2,7 +2,7 @@ var style = document.createElement('style');
 document.head.appendChild(style);
 var sheet = document.styleSheets[0];
 sheet.insertRule(".duolingo { text-align: center; }", sheet.cssRules.length);
-sheet.insertRule(".duolingo-counter { position: relative; bottom: 72px; left: 36px; text-align: center; }", sheet.cssRules.length);
+sheet.insertRule(".duolingo-counter { position: relative; bottom: 43px; left: 29px; text-align: center; }", sheet.cssRules.length);
 var langContent = document.createElement('div');
 langContent.innerHTML = '{htmlcontent}';
 var langBlock = document.getElementById('duolingoBlock');
