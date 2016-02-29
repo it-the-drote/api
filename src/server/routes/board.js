f28b6bae07a47c2ba952c97d333f7b225b9c3c03
like-all <like-all@yandex.com>
2016-02-29 21:48:30 +0300

    Implement CPU metrics
    * Implement CPU metrics
    * Implement CPU metrics

---
+++ src/server/routes/board.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/board/items/cpu', function(req, res) {
+	var randnumber = Math.random() * (100 - 50) + 50;
+	res.send('{ "cpu": ' + randnumber + ' }' );
+});
