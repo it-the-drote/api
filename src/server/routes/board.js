704b1a79b496a49567112801f4a0d4bae36dc324
like-all <like-all@yandex.com>
2016-03-01 14:47:43 +0300

    Fix jshint errors and include fs library

--- src/server/routes/board.js
+++ src/server/routes/board.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.get('/board/items/stats', function(req, res) {
-	var stats = undefined;
+	var stats;
 	try {
 		stats = fs.readFileSync('/tmp/usage.json').toString();
 	} catch(err) {
