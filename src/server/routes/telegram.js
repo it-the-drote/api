722d019f86e550b6369a96ef69b107d13ff30b21
like-all <like-all@yandex.com>
2016-07-04 19:14:18 +0300

    Properly parse chat id

--- src/server/routes/telegram.js
+++ src/server/routes/telegram.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var fs = require('fs');
 var settings = require('/etc/datasources/apps-api.json');
 var leicht = require('/usr/lib/leicht/leicht.js');
-var recipient = fs.readFileSync(settings.pisunBotRecipient, 'utf8').split('\n')[0];
+var recipient = parseInt(fs.readFileSync(settings.pisunBotRecipient, 'utf8').split('\n')[0]);
 
 router.post('/telegram', function(req, res) {
 	console.log(req.body.message);
