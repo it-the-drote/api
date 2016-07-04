ab97cf6057a4ffcc3ee0df94ef1417cfa38b05f2
like-all <like-all@yandex.com>
2016-07-04 18:56:21 +0300

    Fetch default chat id from file

--- src/server/routes/telegram.js
+++ src/server/routes/telegram.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var fs = require('fs');
 var settings = require('/etc/datasources/apps-api.json');
 var leicht = require('/usr/lib/leicht/leicht.js');
+var recipient = fs.readFileSync(settings.pisunBotRecipient, 'utf8').split('\n')[0];
 
 router.post('/telegram', function(req, res) {
 	console.log(req.body.message);
 	if (fs.existsSync('/tmp/pisun.socket')) {
 		if (req.body.token == settings.pisunBotToken) {
-			leicht.sendMessage(settings.pisunBotRecipient, 0, req.body.message, false, "/tmp/pisun.socket");
+			leicht.sendMessage(recipient, 0, req.body.message, false, "/tmp/pisun.socket");
 			res.send("OK\n");
 		} else {
 			res.send("Wrong token\n");
