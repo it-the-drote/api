57b197f0f0c93bc473c0376f166a582340be6bf2
Azer Abdullaev <buckstabu@yandex-team.ru>
2016-08-06 00:08:20 +0300

    Log req.query.repo parameter

--- src/server/routes/webhook.js
+++ src/server/routes/webhook.js
@@ -35,8 +35,7 @@ router.post('/webhook/mdblog', function(req, res) {
 	}
 });
 router.post('/webhook/build-deb', function(req, res){
-	console.log("POST parameters: " + JSON.stringify(req.query, null, '\t'));
-	console.log("Request parameters: " + req.params.repo);
+	console.log("POST parameters: " + req.query.repo);
 	res.send("OK\n");
 });
 
