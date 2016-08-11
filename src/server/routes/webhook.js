cb5179a07b6bf7b03e4273f36558ee1a53d0aae9
Like-all <like-all@yandex.com>
2016-08-11 22:48:29 +0300

    Fix a typo

--- src/server/routes/webhook.js
+++ src/server/routes/webhook.js
@@ -36,7 +36,7 @@ router.post('/webhook/mdblog', function(req, res) {
 });
 router.post('/webhook/build-deb', function(req, res){
 	console.log("POST parameters: " + req.query.repo);
-	console.log("POST body: " + req.body.name;
+	console.log("POST body: " + req.body.name);
 	res.send("OK\n");
 });
 
