ff59d34af54b8a65b8fecfd1e426758120d5d3f3
Like All <like-all@yandex.com>
2017-07-20 00:06:52 -0700

    Added initial cache support

--- src/server/routes/scrobble.js
+++ src/server/routes/scrobble.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Lastfm = require('simple-lastfm');
+var mc = require('mc');
 var settings = require('/etc/datasources/apps-api.json');
 
 var lastfm = new Lastfm({
@@ -11,22 +12,41 @@ var lastfm = new Lastfm({
   debug: true
 });
 
-router.post('/scrobble/report', function(req, res) {
-  console.log('request: %j', req.body.artist);
-  lastfm.getSessionKey(function(result) {
-    console.log('Session key: ' + result.session_key);
-    console.log('request: %j', req);
-    if (result.success) {
-      console.log('Attempting to scrobble...');
-      lastfm.scrobbleTrack({
-        artist: req.body.artist,
-        track: req.body.track,
-        callback: function(result) {
-          console.log('In callback, finished: %j', result);
-          res.send("OK\n");
-        }
-      });
+function scrobble(data, secret, res) {
+  lastfm.scrobbleTrack({
+    artist: data.artist,
+    track: data.track,
+    session_key: secret,
+    callback: function(result) {
+      console.log('In callback, finished: %j', result);
+      res.send("OK\n");
     }
+  });
+}
+
+router.post('/scrobble/report', function(req, res) {
+  var data = {
+    artist: req.body.artist,
+    track: req.body.track
+  }
+  var memcache = new mc.Client();
+  memcache.connect(function () {
+      console.log('Connected to memcached');
+      memcache.get('scrobbler-session-key', function(err, memcacheResponse) {
+        if (err && err.type == 'NOT_FOUND') {
+          lastfm.getSessionKey(function(result) {
+            if (result.success) {
+              memcache.set('scrobbler-session-key', result.session_key, {flags: 0, exptime: 604800}, function(err, status) {
+                console.log('Scrobbler memcache status: ' + status);
+                console.log('Scrobbler memcache error: ' + status);
+              });
+              scrobble(data, result.session_key,res);
+            }
+          });
+        } else {
+          scrobble(data, memcacheResponse['scrobbler-session-key'], res);
+        }
+      })
   });
 });
 
