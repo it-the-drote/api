74250fac4f0d2c71e52de9cba3e1b7d4f9d19fd0
Like All <like-all@yandex.com>
2017-09-20 12:34:59 -0700

    Added package crawler for development repo

---
+++ src/server/background_jobs/addpackages.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+TELEGRAM_TOKEN=`cat /etc/datasources/apps-api.json | jq -r .pisunBotToken`
+LOG_FILE='/home/repo/local/reprepro.log'
+
+cd /home/repo/local
+
+for i in `ls incoming/development/*.deb`; do
+  if dpkg-deb --contents $i > /dev/null 2>&1; then
+    reprepro -Vb . includedeb development $i >> $LOG_FILE
+    curl -XPOST --data-urlencode "token=$TELEGRAM_TOKEN" \
+                --data-urlencode "message=package $i has been successfully added to development repo" \
+                https://api.it-the-drote.tk/telegram >> $LOG_FILE
+  else
+    echo "package $i is broken" >> $LOG_FILE
+    exit 1
+  fi
+done
