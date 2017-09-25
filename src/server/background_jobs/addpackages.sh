23b35a27621561461c307606521d1b4396b5f05f
Like All <like-all@yandex.com>
2017-09-25 02:11:47 -0700

    Enchanced package crawler

--- src/server/background_jobs/addpackages.sh
+++ src/server/background_jobs/addpackages.sh
@@ -9,10 +9,14 @@ for i in `ls incoming/development/*.deb`; do
   if dpkg-deb --contents $i > /dev/null 2>&1; then
     reprepro -Vb . includedeb development $i >> $LOG_FILE
     curl -XPOST --data-urlencode "token=$TELEGRAM_TOKEN" \
-                --data-urlencode "message=package $i has been successfully added to development repo" \
+                --data-urlencode "message=📦✅ package $i has been successfully added to development repo" \
                 https://api.it-the-drote.tk/telegram >> $LOG_FILE
+    rm -f $i
   else
     echo "package $i is broken" >> $LOG_FILE
+    curl -XPOST --data-urlencode "token=$TELEGRAM_TOKEN" \
+                --data-urlencode "message=📦❌ unable to add package $i to development repo" \
+                https://api.it-the-drote.tk/telegram >> $LOG_FILE
     exit 1
   fi
 done
