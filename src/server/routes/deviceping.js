c0b2cbd5ea59d63e51d08ba0e2a850a7111161aa
Like All <like-all@yandex.com>
2017-09-26 02:23:48 -0700

    Added extra logging

--- src/server/routes/deviceping.js
+++ src/server/routes/deviceping.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var fs = require('fs');
 var settings = require('/etc/datasources/apps-api.json');
 
-router.post('/device-ping/:device_uuid',function(req, res) {
+router.post('/device-ping/:device_uuid', function(req, res) {
   console.log(req.params.device_uuid);
+  console.log("Blah");
   res.send("OK\n");
 });
 
