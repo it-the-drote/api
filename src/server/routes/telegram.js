24b5349f89554a8fc2b5d9054c0489535554b939
Like All <like-all@yandex.com>
2018-02-06 04:18:02 -0800

    Updated chat routing of telegram endpoint

--- src/server/routes/telegram.js
+++ src/server/routes/telegram.js
@@ -3,13 +3,15 @@ var router = express.Router();
 var fs = require('fs');
 var settings = require('/etc/datasources/apps-api.json');
 var leicht = require('/usr/lib/leicht/leicht.js');
-var recipient = parseInt(fs.readFileSync(settings.pisunBotRecipient, 'utf8').split('\n')[0]);
+var environment = parseString(fs.readFileSync('/etc/apps/environment.type', 'utf8').split('\n')[0]);
+var recipient = parseInt(fs.readFileSync('/etc/datasources/pisun-default-chat.' + environment, 'utf8').split('\n')[0]);
+var socket = '/var/run/apps/pisun.socket';
 
 router.post('/telegram', function(req, res) {
 	console.log(req.body.message);
-	if (fs.existsSync('/tmp/pisun.socket')) {
+	if (fs.existsSync(socket)) {
 		if (req.body.token == settings.pisunBotToken) {
-			leicht.sendMessage(recipient, 0, req.body.message, false, "/var/run/pisun.socket");
+			leicht.sendMessage(recipient, 0, req.body.message, false, socket);
 			res.send("OK\n");
 		} else {
 			res.send("Wrong token\n");
