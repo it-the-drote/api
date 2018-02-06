07405cc7f15c18e8e34241294e6ce70eeac5a36e
Like All <like-all@yandex.com>
2018-02-06 05:03:17 -0800

    Removed hardcoded path definitions from telegram endpoint

--- src/server/routes/telegram.js
+++ src/server/routes/telegram.js
@@ -5,13 +5,12 @@ var settings = require('/etc/datasources/apps-api.json');
 var leicht = require('/usr/lib/leicht/leicht.js');
 var environment = fs.readFileSync('/etc/apps/environment.type', 'utf8').split('\n')[0];
 var recipient = parseInt(fs.readFileSync('/etc/datasources/pisun-default-chat.' + environment, 'utf8').split('\n')[0]);
-var socket = '/var/run/apps/pisun.socket';
 
 router.post('/telegram', function(req, res) {
 	console.log(req.body.message);
-	if (fs.existsSync(socket)) {
+	if (fs.existsSync(settings.pisunSocketPath)) {
 		if (req.body.token == settings.pisunBotToken) {
-			leicht.sendMessage(recipient, 0, req.body.message, false, socket);
+			leicht.sendMessage(recipient, 0, req.body.message, false, settings.pisunSocketPath);
 			res.send("OK\n");
 		} else {
 			res.send("Wrong token\n");
