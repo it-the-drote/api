13bdf91e31ceedf6aa0ea46f23bbd472606f1e13
Like All <like-all@yandex.com>
2018-10-31 22:08:15 +0100

    Handling socket removal within systemd service

--- src/bin/server.js
+++ src/bin/server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var url = require('url');
-var fs = require('fs');
 var utils = ('./utilities');
 var oldUmask = process.umask(0000);
 
@@ -27,13 +26,4 @@ var server = http.createServer(function(request, response) {
 server.listen('/var/run/apps/apps-api.sock', function() {
   process.umask(oldUmask);
   console.log('Server bound');
-});
-
-server.on('error', function (e) {
-  if (e.code == 'EADDRINUSE') {
-    console.log('Address in use, retrying...');
-    setTimeout(function () {
-      fs.unlink('/var/run/apps/apps-api.sock');
-    }, 1000);
-  }
 });
