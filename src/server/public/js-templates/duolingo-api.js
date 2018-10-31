60d8a08d9ccb3295dc2bee6ae8d4af94cdfe5e03
Like All <like-all@yandex.com>
2018-10-31 16:52:40 +0100

    Removed legacy code

--- src/server/public/js-templates/duolingo-api.js
+++
@@ -1,9 +0,0 @@
-var style = document.createElement('style');
-document.head.appendChild(style);
-var sheet = document.styleSheets[0];
-sheet.insertRule(".duolingo { text-align: center; }", sheet.cssRules.length);
-sheet.insertRule(".duolingo-counter { position: relative; bottom: 43px; left: 29px; text-align: center; }", sheet.cssRules.length);
-var langContent = document.createElement('div');
-langContent.innerHTML = '{htmlcontent}';
-var langBlock = document.getElementById('duolingoBlock');
-langBlock.appendChild(langContent);
