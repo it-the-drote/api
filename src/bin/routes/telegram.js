95ddf80f196bc25ec8865781635cd85f6d44652e
Like All <like-all@yandex.com>
2018-11-12 16:48:30 +0100

    Handling GET and POST requests within telegram endpoint

--- src/bin/routes/telegram.js
+++ src/bin/routes/telegram.js
@@ -1,4 +1,15 @@
+var fs = require('fs');
+var telegramToken = process.env['TELEGRAM_BOT_TOKEN'];
+var leicht = require('/usr/lib/leicht/leicht.js');
+var recipient = parseInt(fs.readFileSync(process.env['TELEGRAM_BOT_RECIPIENT'], 'utf8').split('\n')[0]);
+
 module.exports = (request, response) => {
+  if (request.method === 'GET') {
     response.writeHead(200);
-    response.end("API telegram page mock");
+    response.end("This endpoint does not support GET requests");
+  } else if (request.method === 'POST') {
+    console.log(request.body.message);
+    response.writeHead(200);
+    response.end("OK");
+  }
 };
