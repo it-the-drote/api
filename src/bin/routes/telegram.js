4cf39cda9ce4133662fe21e53b1ac472e6e13777
Like All <like-all@yandex.com>
2018-11-12 17:21:42 +0100

    Debugging incoming data stream

--- src/bin/routes/telegram.js
+++ src/bin/routes/telegram.js
@@ -9,8 +9,14 @@ module.exports = (request, response) => {
     response.writeHead(200);
     response.end("This endpoint does not support GET requests");
   } else if (request.method === 'POST') {
-    console.log("Request structure: " + util.inspect(request));
-    response.writeHead(200);
-    response.end("OK");
+    var data = '';
+    request.on('data', (chunk) => {
+      data += chunk;
+    };
+    request.on('end', () => {
+      console.log("Message structure: " + util.inspect(data));
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end('Success');
+    };
   }
 };
